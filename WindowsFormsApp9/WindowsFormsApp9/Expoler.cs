470d9d5249c823dd4b6c0792123758b0de0baba1
lRaika <arnurelnur@gmail.com>
2017-10-11 17:24:04 +0600

    HomeWork
    HomeWork(1/2)Done

---
+++ WindowsFormsApp9/WindowsFormsApp9/Expoler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp9
+{
+    public partial class Expoler : Form
+    {
+        public Expoler()
+        {
+            InitializeComponent();
+            foreach (DriveInfo d in DriveInfo.GetDrives())
+            {
+                TreeNode driveNode = new TreeNode { Text = d.Name };
+                string[] directories = Directory.GetDirectories(d.Name);
+                foreach (string director in directories)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = director.Remove(0, director.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+                treeView1.Nodes.Add(driveNode);
+            }
+        }
+
+        
+    }
+}
